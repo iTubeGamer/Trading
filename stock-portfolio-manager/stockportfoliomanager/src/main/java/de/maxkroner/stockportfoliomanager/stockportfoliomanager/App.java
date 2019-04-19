4ba083341307a352f4e87a435b6c613c40d3396a
Max Kroner <email@example.com>
2019-04-19 21:11:55 +0200

    initial commit for stock prices backend

---
+++ stock-portfolio-manager/stockportfoliomanager/src/main/java/de/maxkroner/stockportfoliomanager/stockportfoliomanager/App.java
@@ -0,0 +1,13 @@
+package de.maxkroner.stockportfoliomanager.stockportfoliomanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
